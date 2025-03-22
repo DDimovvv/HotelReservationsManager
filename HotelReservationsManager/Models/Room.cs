5c8b9939beb1561bb97da7acaba35658b8850ad5
ИТ кариера 12 <  12@DESKTOP-4CKJCET>
2025-03-22 11:37:07 +0200

    Add project files.

---
+++ HotelReservationsManager/Models/Room.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_Reservations_Manager.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomNumber { get; set; }
+        [Range(2, 10)]
+        [Required]
+        public int Capacity { get; set; }
+        public enum RoomTypes { Two_Single_Beds, Apartment, Double_Bed, Penthouse, Maissonette }
+        public RoomTypes RoomType { get; set; }
+        public bool IsAvailable { get; set; }
+        [Required]
+        public double AdultPrice { get; set; }
+        [Required]
+        public double ChildPrice { get; set; }
+        public ICollection<Reservation>? Reservations { get; set; }
+    }
+}
---
+++ HotelReservationsManager/Models/User.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_Reservations_Manager.Models
+{
+    public class User : IdentityUser
+    {
+        [Key]
+        public Guid EGN { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string MiddleName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        public DateOnly HireDate { get; set; }
+        public bool Active { get; set; }
+        public DateOnly? ReleaseDate { get; set; } = null;
+    }
+}
